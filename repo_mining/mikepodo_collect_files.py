7b76e4dc9007ecb3249b345520fd64eb96088b6d
Michael Podolsky <michaelspodolsky@gmail.com>
2026-08-28 10:30:54 -0700

    use personal access token and filter by source files

--- repo_mining/mikepodo_collect_files.py
+++ repo_mining/mikepodo_collect_files.py
@@ -1,39 +1,49 @@
 import json
 import requests
 import csv
-
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).resolve().parent / ".env")
 
 if not os.path.exists("data"):
  os.makedirs("data")
 
 # GitHub Authentication function
-def github_auth(url, lsttoken, ct):
+def github_auth(url, token):
     jsonData = None
     try:
-        ct = ct % len(lstTokens)
-        headers = {'Authorization': 'Bearer {}'.format(lsttoken[ct])}
+        headers = {'Authorization': 'Bearer {}'.format(token)}
         request = requests.get(url, headers=headers)
         jsonData = json.loads(request.content)
-        ct += 1
     except Exception as e:
         pass
         print(e)
-    return jsonData, ct
+    return jsonData
+
+def is_source(filename):
+    SOURCE_EXTENSIONS = [
+        ".java",
+        ".kt",
+        ".cpp",
+        ".h"
+    ]
+    return any(filename.endswith(ext) for ext in SOURCE_EXTENSIONS)
 
 # @dictFiles, empty dictionary of files
-# @lstTokens, GitHub authentication tokens
+# @token, GitHub authentication token
 # @repo, GitHub repo
-def countfiles(dictfiles, lsttokens, repo):
+def countfiles(dictfiles, token, repo):
     ipage = 1  # url page counter
-    ct = 0  # token counter
 
     try:
         # loop though all the commit pages until the last returned empty page
         while True:
             spage = str(ipage)
             commitsUrl = 'https://api.github.com/repos/' + repo + '/commits?page=' + spage + '&per_page=100'
-            jsonCommits, ct = github_auth(commitsUrl, lsttokens, ct)
+            jsonCommits = github_auth(commitsUrl, token)
 
             # break out of the while loop if there are no more commits in the pages
             if len(jsonCommits) == 0:
@@ -43,12 +53,16 @@ def countfiles(dictfiles, lsttokens, repo):
                 sha = shaObject['sha']
                 # For each commit, use the GitHub commit API to extract the files touched by the commit
                 shaUrl = 'https://api.github.com/repos/' + repo + '/commits/' + sha
-                shaDetails, ct = github_auth(shaUrl, lsttokens, ct)
+                shaDetails = github_auth(shaUrl, token)
                 filesjson = shaDetails['files']
                 for filenameObj in filesjson:
                     filename = filenameObj['filename']
-                    dictfiles[filename] = dictfiles.get(filename, 0) + 1
-                    print(filename)
+
+                    if is_source(filename):
+                        dictfiles[filename] = dictfiles.get(filename, 0) + 1
+                        print("adding " + filename)
+                    else:
+                        print("skipping " + filename + ' - not a source file')
             ipage += 1
     except:
         print("Error receiving data")
@@ -59,17 +73,12 @@ repo = 'scottyab/rootbeer'
 # repo = 'k9mail/k-9' # This repo is commit heavy. It takes long to finish executing
 # repo = 'mendhak/gpslogger'
 
-
-# put your tokens here
-# Remember to empty the list when going to commit to GitHub.
-# Otherwise they will all be reverted and you will have to re-create them
-# I would advise to create more than one token for repos with heavy commits
-lstTokens = ["fd02a694b606c4120b8ca7bbe7ce29229376ee",
-                "16ce529bdb32263fb90a392d38b5f53c7ecb6b",
-                "8cea5715051869e98044f38b60fe897b350d4a"]
+token = os.getenv("MIKEPODO_GH_TOKEN")
+if not token:
+    raise SystemExit("Set MIKEPODO_GH_TOKEN in repo_mining/.env")
 
 dictfiles = dict()
-countfiles(dictfiles, lstTokens, repo)
+countfiles(dictfiles, token, repo)
 print('Total number of files: ' + str(len(dictfiles)))
 
 file = repo.split('/')[1]
